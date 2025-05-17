bc2fc1dcfd39989d35bbb5a19c4da9a3ce399c44
Eli Chapman <eychapman@gmail.com>
2025-05-17 08:03:27 -0500

    Make it scroll to top on page change

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Donate from "./pages/Donate";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
